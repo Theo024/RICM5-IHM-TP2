d88423d5921dfa1a23e61e75bbba2d11480b918e
Théo Lévesque <theo.levesque.024@gmail.com>
2018-10-05 11:51:02 +0200

    Color picker working

--- ihm/Paint.java
+++ ihm/Paint.java
@@ -17,15 +17,18 @@ import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Rectangle2D;
 import java.util.Vector;
 
 import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 import javax.swing.event.MouseInputListener;
@@ -34,7 +37,49 @@ import javax.swing.event.MouseInputListener;
 
 @SuppressWarnings("serial")
 class Paint extends JFrame {
-    Vector<Shape> shapes = new Vector<>();
+    Vector<Shape> shapes        = new Vector<>();
+    Vector<Color> colors        = new Vector<>();
+    Color         colorSelected = Color.BLACK;
+
+    class ColorButton extends JButton implements MouseListener {
+        String name;
+        Color  color;
+
+        public ColorButton(String name, int r, int g, int b) {
+            this.name = name;
+            color = new Color(r, g, b);
+            setBackground(color);
+            setText("    ");
+            addMouseListener(this);
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            System.out.println("color selected - " + color);
+            colorSelected = color;
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+
+    }
+
+    ColorButton[] colorButtons = { new ColorButton("black", 0, 0, 0), new ColorButton("red", 255, 0, 0),
+            new ColorButton("green", 0, 255, 0), new ColorButton("blue", 0, 0, 255) };
 
     class Tool extends AbstractAction implements MouseInputListener {
         Point o;
@@ -93,6 +138,7 @@ class Paint extends JFrame {
                      path = new Path2D.Double();
                      path.moveTo(o.getX(), o.getY());
                      shapes.add(shape = path);
+                     colors.add(colorSelected);
                  }
                  path.lineTo(e.getX(), e.getY());
                  panel.repaint();
@@ -104,6 +150,7 @@ class Paint extends JFrame {
                  if (rect == null) {
                      rect = new Rectangle2D.Double(o.getX(), o.getY(), 0, 0);
                      shapes.add(shape = rect);
+                     colors.add(colorSelected);
                  }
                  rect.setRect(Math.min(e.getX(), o.getX()), Math.min(e.getY(), o.getY()), Math.abs(e.getX() - o.getX()),
                          Math.abs(e.getY() - o.getY()));
@@ -116,6 +163,7 @@ class Paint extends JFrame {
                  if (ellipse == null) {
                      ellipse = new Ellipse2D.Double(o.getX(), o.getY(), 0, 0);
                      shapes.add(shape = ellipse);
+                     colors.add(colorSelected);
                  }
                  ellipse.setFrame(Math.min(e.getX(), o.getX()), Math.min(e.getY(), o.getY()),
                          Math.abs(e.getX() - o.getX()), Math.abs(e.getY() - o.getY()));
@@ -136,6 +184,14 @@ class Paint extends JFrame {
                     add(tool);
             }
         }, BorderLayout.NORTH);
+
+        add(new JToolBar(SwingConstants.VERTICAL) {
+            {
+                for (JButton button : colorButtons)
+                    add(button);
+            }
+        }, BorderLayout.WEST);
+
         add(panel = new JPanel() {
             @Override
             public void paintComponent(Graphics g) {
@@ -146,9 +202,10 @@ class Paint extends JFrame {
                 g2.setColor(Color.WHITE);
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
-                g2.setColor(Color.BLACK);
-                for (Shape shape : shapes)
-                    g2.draw(shape);
+                for (int i = 0; i < shapes.size(); i++) {
+                    g2.setColor(colors.get(i));
+                    g2.draw(shapes.get(i));
+                }
             }
         });
 
